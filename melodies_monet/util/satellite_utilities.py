0842d163b464f1bdc06ab9b7cd8525c87f2384d4
Maggie Bruckner <maggie.bruckner@noaa.gov>
2024-12-17 15:22:42 -0700

    Support for regional models and MOPITT pairing

--- melodies_monet/util/satellite_utilities.py
+++ melodies_monet/util/satellite_utilities.py
@@ -54,7 +54,7 @@ def check_timestep(model_data,obs_data):
         print('Timestep check and model resample failed')
         raise
 
-def mopitt_l3_pairing(model_data,obs_data,co_ppbv_varname):
+def mopitt_l3_pairing(model_data,obs_data,co_ppbv_varname,global_m=True):
     ''' Calculate model CO column, with MOPITT averaging kernel applied.
     '''
     import xarray as xr
@@ -78,7 +78,7 @@ def mopitt_l3_pairing(model_data,obs_data,co_ppbv_varname):
         raise
     # initialize regridder for horizontal interpolation 
     # from model grid to MOPITT grid
-    grid_adjust = xe.Regridder(model_obstime[['latitude','longitude']],obs_data[['lat','lon']],'bilinear',periodic=True)
+    grid_adjust = xe.Regridder(model_obstime[['latitude','longitude']],obs_data[['lat','lon']],'bilinear',periodic=global_m,unmapped_to_nan=True)
     co_model_regrid = grid_adjust(model_obstime[co_ppbv_varname])
     pressure_model_regrid = grid_adjust(model_obstime['pres_pa_mid']/100.)
     
@@ -98,7 +98,7 @@ def mopitt_l3_pairing(model_data,obs_data,co_ppbv_varname):
     log_mod = np.log10(co_regrid)
     diff_arr = log_mod-log_ap
     ## smooth/apply ak
-    smoothed = obs_data['apriori_col'] + (obs_data['ak_col']*diff_arr).sum(dim='alt')
+    smoothed = obs_data['apriori_col'] + (obs_data['ak_col']*diff_arr).sum(dim='alt',skipna=False)
     
     # Add variable name to smoothed model dataarray, combine with obs_data
     smoothed = smoothed.rename(co_ppbv_varname+'_column_model')
